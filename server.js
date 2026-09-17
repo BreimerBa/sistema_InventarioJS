c08ae974bb4a67af8ac91cffa798a3f18d4f2b24
BreimerBa <breimerba@gmail.com>
2026-09-17 16:07:50 -0500

    produccion

--- server.js
+++ server.js
@@ -1,14 +1,3 @@
-/* //Conexion a la base de datos, puesto 3000
-
-const app = require("./src/main/js/app");
-
-const PORT = 3000;
-
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
- */
-
 require('dotenv').config();
 const app = require('./src/main/js/app');
 
--- src/main/js/config/database.js
+++ src/main/js/config/database.js
@@ -1,32 +1,3 @@
-/* const mysql = require("mysql2/promise");
-
-const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "inventario_web",
-
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
-
-async function probarConexion() {
-    try {
-        const connection = await pool.getConnection();
-
-        console.log("¡Conectado a MySQL exitosamente!");
-
-        connection.release();
-    } catch (error) {
-        console.error("Error al conectar con MySQL:", error.message);
-    }
-}
-
-probarConexion();
-
-module.exports = pool; */
-
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../../../../.env') });
 
