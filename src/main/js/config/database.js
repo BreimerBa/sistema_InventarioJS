4856a515af5574adb13d3aff0f81b67e93664fb3
BreimerBa <breimerba@gmail.com>
2026-09-17 16:37:36 -0500

    database

--- src/main/js/config/database.js
+++ src/main/js/config/database.js
@@ -1,17 +1,38 @@
-const path = require('path');
+/* const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../../../../.env') });
 
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '1234',
+    host: process.env.DB_HOST || '',
+    user: process.env.DB_USER || '',
+    password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'inventario_web',
     port: process.env.DB_PORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
+});
+
+module.exports = pool; */
+
+const path = require('path');
+
+require('dotenv').config({ path: path.resolve(__dirname, '../../../../.env') });
+
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT) || 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+
+    ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false
 });
 
 module.exports = pool;
